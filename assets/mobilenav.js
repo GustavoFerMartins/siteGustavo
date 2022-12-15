3f7479ab37703a99fe3b81628ba9db46ca8618dd
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2022-12-15 13:13:47 -0300

    Initial commit

---
+++ assets/mobilenav.js
@@ -0,0 +1,43 @@
+class MobileNavbar{
+   constructor(mobileMenu, navItems, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navItems = document.querySelector(navItems);
+    this.navLinks = document.querySelectorAll(navLinks)
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+   } 
+
+   animateItems() {
+    this.navLinks.forEach((Link, index) => {
+        Link.style.animation
+            ? (Link.style.animation = "")
+            : (Link.style.animation = `navItemsFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+    });
+   }
+
+
+   handleClick(){
+    this.navItems.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateItems();
+   }
+
+   addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+   }
+
+   init(){
+    if(this.mobileMenu){
+        this.addClickEvent();
+    }
+    return this;
+   }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    "#items",
+    "#items li",
+);
+
+mobileNavbar.init();
