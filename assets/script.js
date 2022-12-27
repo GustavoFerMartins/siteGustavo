f6b7d9c12d664ae8ec3295b8ddb7c9ff0c9bc8cd
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2022-12-27 10:32:34 -0300

    Atualiza

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,11 @@
 Index
 Progresso Curso */
 const progresso = document.querySelector(".porcentagem div")
-progresso.setAttribute("style", "width: 42%")
+progresso.setAttribute("style", "width: 100%")
+
+/* 
+Index
+Progresso Curso Dois*/
+
+const progressodois = document.querySelector(".porcentagem-dois div")
+progressodois.setAttribute("style", "width: 5%")
